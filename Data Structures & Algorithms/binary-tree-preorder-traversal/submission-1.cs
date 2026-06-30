7c8d8105e1f4cce7f8dd8762582da881adbb091c
Rishabh Dev Dogra <105988215+RishabhDevDogra@users.noreply.github.com>
2026-06-30 20:37:48 +0530

    Update: binary-tree-preorder-traversal - submission-1

--- Data Structures & Algorithms/binary-tree-preorder-traversal/submission-1.cs
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-1.cs
@@ -12,46 +12,44 @@
  * }
  */
 public class Solution {
-    public List<int> InorderTraversal(TreeNode root) {
+    public List<int> PreorderTraversal(TreeNode root) {
         List<int> ans = new();
-
+        if(root == null){
+            return ans;
+        }
         Stack<TreeNode> stack = new();
-        TreeNode current = root;
-        while(true)
+        stack.Push(root);
+        while(stack.Count>0)
         {
-            if(current!=null)
-            {
-                stack.Push(current);
-                current = current.left;
-            }
-            else
-            {
-                if(stack.Count==0)
-                {
-                    break;
-                }
-                current = stack.Pop();
-                ans.Add(current.val);
-                
-                current = current.right;
-            }
+            TreeNode current = stack.Pop();
+            ans.Add(current.val);
+
+            if(current.right!=null)
+            stack.Push(current.right);
+
+            if(current.left!=null)
+            stack.Push(current.left);
+            
         }
         return ans;
         
     }
+    //public List<int> PreorderTraversal(TreeNode root) {
+      //      List<int> ans = new();
 
-    //public IList<int> InorderTraversal(TreeNode root) {
-   //     List<int> ans = new();
-   //     DFS(root,ans);
-   //     return ans;
-  // }
-    //private void DFS(TreeNode root, List<int> ans)
-    //{
-   //     if(root == null){
-    //        return;
-    //    }
-   //     DFS(root.left,ans);
-    //    ans.Add(root.val);
-   //     DFS(root.right,ans);
-   // }
+        //    DFS(root,ans);
+
+        //    return ans;
+      //  }
+
+    private void DFS(TreeNode root, List<int> ans)
+    {
+        if(root == null)
+        {
+        return;
+        }
+        ans.Add(root.val);
+        DFS(root.left, ans);
+        DFS(root.right, ans);
+    }
 }
